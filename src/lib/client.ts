830af45fbe66176d036bacb0ea0232ff4f087201
JarneDM <jarne.demeyer05@hotmail.com>
2026-01-07 00:39:24 +0100

    refactor: simplify Prisma client initialization by removing unused adapter and connection pooling

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,13 +1,6 @@
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
-let prisma = new PrismaClient();
-
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
