8234f4306be07a55b57e744134accebefbe04e90
JarneDM <jarne.demeyer05@hotmail.com>
2026-02-08 12:56:59 +0100

    feat: update getLeaderboard function to exclude specific user and adjust leaderboard retrieval

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -5,7 +5,8 @@ export const LeaderboardCommand = {
   data: new SlashCommandBuilder().setName("leaderboard").setDescription("Toon de voice time leaderboard"),
 
   async execute(interaction) {
-    const topUsers = await getLeaderboard();
+    const excludedUserId = "1131728345931972649";
+    const topUsers = await getLeaderboard(10, excludedUserId);
 
     const leaderboardText = await Promise.all(
       topUsers.map(async (entry, index) => {
